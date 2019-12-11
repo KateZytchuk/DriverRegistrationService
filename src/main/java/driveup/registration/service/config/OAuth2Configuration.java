f6f4ad6ec0e357d5272f2522af919cfed39bb48d
zaets39 <katezytchuk24@gmail.com>
2019-12-11 17:44:01 +0200

    some changes

--- src/main/java/driveup/registration/service/config/OAuth2Configuration.java
+++ src/main/java/driveup/registration/service/config/OAuth2Configuration.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.
 
 @PropertySource("clientInfo.properties")
 @Configuration
--- src/main/java/driveup/registration/service/config/SecurityConfiguration.java
+++ src/main/java/driveup/registration/service/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package driveup.registration.service.config;
 
 
+import driveup.registration.service.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
